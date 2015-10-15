3102bc9831fc2e6ca81719d153c074dc24b29143
Brian Woodward <brian.woodward@gmail.com>
2015-10-15 13:51:59 -0400

    tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,19 @@
+import fs from 'fs';
+import path from 'path';
 import assert from 'assert';
-import app from '../index';
+import update from '../index';
 
-describe('cyborg', () => {
-  it('should pass a test', () => {
-    assert.equal(app('bar'), 'foo bar');
+describe('update-contributors', function() {
+  this.timeout(0);
+
+  it('should get contributors', function(done) {
+    let pkg = JSON.parse(fs.readFileSync(path.join(__dirname, 'fixtures/composer-package.json')));
+    update(pkg, (err, results) => {
+      if (err) return done(err);
+      assert(results);
+      assert(Array.isArray(results.contributors));
+      assert(results.contributors.length > 0);
+      done();
+    });
   });
 });
