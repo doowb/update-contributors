68152928c4a6ffde6b1ea6af441a909e0c4dfdd3
Brian Woodward <brian.woodward@gmail.com>
2015-10-23 10:28:54 -0400

    updating comments in utils

--- bin/utils.js
+++ bin/utils.js
@@ -1,8 +1,29 @@
 'use strict';
 
+/**
+ * Module dependencies
+ */
+
 var utils = require('lazy-cache')(require);
+
+/**
+ * Temporarily re-assign `require` to trick browserify and
+ * webpack into reconizing lazy dependencies.
+ *
+ * This tiny bit of ugliness has the huge dual advantage of
+ * only loading modules that are actually called at some
+ * point in the lifecycle of the application, whilst also
+ * allowing browserify and webpack to find modules that
+ * are depended on but never actually called.
+ */
+
 var fn = require;
 require = utils;
+
+/**
+ * Lazily required module dependencies
+ */
+
 require('cwd');
 require('async');
 require('load-pkg', 'pkg');
@@ -11,5 +32,15 @@ require('ansi-gray', 'gray');
 require('write', 'writeFile');
 require('ansi-green', 'green');
 require('object.omit', 'omit');
+
+/**
+ * Restore `require`
+ */
+
 require = fn;
+
+/**
+ * Expose `utils` modules
+ */
+
 module.exports = utils;
